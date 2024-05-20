ee5cbcb1b08cc777b4dc8fecd3b0fa143b3fa794
Toyehamzat <anibiseun@gmail.com>
2024-05-20 12:32:37 +0100

    feat: Add functionality to submit messages and display them on the home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { formatDistanceToNow } = require("date-fns");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "toye",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "anibi",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", function (req, res, next) {
+  const data = req.body;
+  messages.push({
+    text: data.message,
+    user: data.user,
+    added: formatDistanceToNow(new Date(), { addSuffix: true }),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
