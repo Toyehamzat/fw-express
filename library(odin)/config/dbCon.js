0dbf1bfd182fac4d5625d8a8a72c1b9be6ab4b55
Toyehamzat <anibiseun@gmail.com>
2024-05-28 15:00:45 +0100

    refactor: Update MongoDB connection string in dbCon.js

--- library(odin)/config/dbCon.js
+++ library(odin)/config/dbCon.js
@@ -2,13 +2,10 @@
 const mongoose = require("mongoose");
 
 const mongoDB =
-  "mongodb+srv://tazmaheyot:1KoXs7tLYjapdQ7D@cluster0.bymjqjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://tazmaheyot:1KoXs7tLYjapdQ7D@cluster0.bymjqjo.mongodb.net/localLibraryDB?retryWrites=true&w=majority&appName=Cluster0";
 const ConnectDb = async () => {
   try {
-    await mongoose.connect(mongoDB, {
-      // useUnifiedTopology: true,
-      // useNewUrlParser: true,
-    });
+    await mongoose.connect(mongoDB);
   } catch (err) {
     console.log(err);
   }
