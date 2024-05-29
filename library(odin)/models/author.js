14a0494ccfa4d5b838a4388df1964dd2a6093f89
Toyehamzat <anibiseun@gmail.com>
2024-05-29 15:02:18 +0100

    feat: Add date formatting to author and bookInstance models

--- library(odin)/models/author.js
+++ library(odin)/models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
@@ -27,5 +27,15 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
