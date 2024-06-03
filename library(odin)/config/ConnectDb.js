02797f8a0e2a122d5558608b72eb1d45954998aa
Toyehamzat <anibiseun@gmail.com>
2024-06-03 16:03:51 +0100

    refactor: Add ConnectDb module to handle MongoDB connection

---
+++ library(odin)/config/ConnectDb.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const mongoDB =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://anibiseun:6kn9uNOCzFPdHaac@cluster0.yftm9oz.mongodb.net/n?retryWrites=true&w=majority&appName=Cluster0";
+
+const ConnectDb = async () => {
+  try {
+    await mongoose.connect(mongoDB, {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+module.exports = ConnectDb;
