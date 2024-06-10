3fb0b5f6905bb4f9ccda2d0877df1772a6e3d247
Toyehamzat <anibiseun@gmail.com>
2024-06-10 13:03:25 +0100

    refactor: Update bookInstance_delete_get in bookInstanceController

--- library(odin)/controllers/bookInstanceController.js
+++ library(odin)/controllers/bookInstanceController.js
@@ -96,12 +96,25 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstance");
+  }
+
+  res.render("bookInstance_delete", {
+    title: "Delete Book instance",
+    bookinstance: bookInstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  await BookInstance.findByIdAndDelete(req.body.bookinstanceid).exec();
+  res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
